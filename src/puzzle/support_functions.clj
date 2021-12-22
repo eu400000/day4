7617e77d0b4d65e8e4dfa7aa8ff601cf6e08089b
eu400000 <59219148+eu400000@users.noreply.github.com>
2021-12-22 18:07:45 +0100

    Implemented checkForBingo, checkForBingoHor, checkForBingoVer

--- src/puzzle/support_functions.clj
+++ src/puzzle/support_functions.clj
@@ -22,7 +22,6 @@
   (map #(Integer/parseInt %1) (str/split (first inputData) #",")))
 
 (defn convertStringWithIntegerToIntegerList
-  ;; todo rename function
   "Assumes max a 2 spaces next to each other"
   [stringWithIntegers]
   (let [stringWithSingleSpaces (str/replace stringWithIntegers #"  " " ")
@@ -44,17 +43,11 @@
         (flatten acc)
         ;;using into to make the LazySeq from repeat into a PersistentList (faciltates easier unit test)
         (recur (drop 25 boardData) (list acc (hash-map :boardName (str "board" id) :board (getNextBoard boardData) :boardHits (into () (repeat 25 0)))) (inc id))))))
-        
 
 (defn updateBoardHits
   "Handles 1 board at a time"
   [board boardHits drawnNumber]
-  (if (= drawnNumber -1) ;-1 means initiatize boardHits
-    (repeat 25 0)
-    
-    
-    (map #(if (= %1 drawnNumber) (inc %2) %2) board boardHits)
-  ))
+  (map #(if (= %1 drawnNumber) (inc %2) %2) board boardHits))
 
 (defn updateAllBoardHits
   "Handles all boards"
@@ -63,4 +56,45 @@
                                (updateBoardHits (:board %) (:boardHits %) drawnNumber)))
        boards))
 
+(defn checkForBingoHor
+  [boardHits]
+  (loop [currentHits boardHits]
+    (if (= (apply + (take 5 currentHits)) 5)
+      true
+      (if (empty? currentHits)
+        false
+        (recur (drop 5 currentHits))))))
 
+(defn checkForBingoVer
+  [boardHits]
+  (loop [currentHits boardHits
+         column 1]
+    (if (= (apply + (take-nth 5 currentHits)) 5)
+      true
+      (if (= column 5)
+        false
+        (recur (drop 1 currentHits) (inc column))))))
+
+(defn checkForBingo
+  [boards]
+  (loop [currentBoards boards]
+    (let [currentBoard (first currentBoards)
+          currentBoardHits (:boardHits currentBoard)]
+      (if (or (= (checkForBingoHor currentBoardHits) true)
+              (= (checkForBingoVer currentBoardHits) true))
+        (:boardName currentBoard)
+        (if (empty? currentBoards)
+          nil
+          (recur (drop 1 currentBoards))))))
+)
+
+
+  (defn processDrawnNumbers
+    [boards drawnNumbers]
+    (loop [currentBoards boards
+           remainingNumbers drawnNumbers]
+      ;; (printvar currentBoards)
+      ;; (printvar remainingNumbers)
+      (if (zero? (count remainingNumbers))
+        currentBoards
+        (recur (updateAllBoardHits currentBoards (first remainingNumbers)) (drop 1 remainingNumbers)))))
--- test/puzzle/support_functions_test.clj
+++ test/puzzle/support_functions_test.clj
@@ -13,6 +13,14 @@
                      {:boardHits (0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0), :boardName "board2", :board (3 15 0 2 22 9 18 13 17 5 19 8 7 25 23 20 11 10 24 4 14 21 16 12 6)}
                      {:boardHits (0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0), :boardName "board3", :board (14 21 17 24 4 10 16 15 9 19 18 8 23 26 20 22 11 13 6 5 2 0 12 3 7)}))
 
+(def boardHitsBingoHor '({:boardHits (1 1 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0), :boardName "board1", :board (22 13 17 11 0 8 2 23 4 24 21 9 14 16 7 6 10 3 18 5 1 12 20 15 19)}
+                         {:boardHits (0 0 0 0 0 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0), :boardName "board2", :board (3 15 0 2 22 9 18 13 17 5 19 8 7 25 23 20 11 10 24 4 14 21 16 12 6)}
+                         {:boardHits (0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0), :boardName "board3", :board (14 21 17 24 4 10 16 15 9 19 18 8 23 26 20 22 11 13 6 5 2 0 12 3 7)}))
+(def boardHitsBingoVer '({:boardHits (1 0 1 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0), :boardName "board1", :board (22 13 17 11 0 8 2 23 4 24 21 9 14 16 7 6 10 3 18 5 1 12 20 15 19)}
+                         {:boardHits (0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0), :boardName "board2", :board (3 15 0 2 22 9 18 13 17 5 19 8 7 25 23 20 11 10 24 4 14 21 16 12 6)}
+                         {:boardHits (0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0), :boardName "board3", :board (14 21 17 24 4 10 16 15 9 19 18 8 23 26 20 22 11 13 6 5 2 0 12 3 7)}))
+
+
 (deftest loadInputData-test
   (is (= puzzleTestData (loadInputData "resources/puzzleTestInput.txt"))))
 
@@ -32,8 +40,22 @@
   (is (= '(22 13 17 11  0  8  2 23  4 24 21  9 14 16  7  6 10  3 18  5  1 12 20 15 19) (getNextBoard allBoardData))))
 
 (deftest updateBoardHits-test
-  (is (= '(1 0 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0) (updateBoardHits '(22 13 17 11 0 8 2 23 4 24 21 9 14 16 7 6 10 3 18 5 1 12 20 15 19) '(1 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0) 17)))
-  (is (= '(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0) (updateBoardHits '(22 13 17 11 0 8 2 23 4 24 21 9 14 16 7 6 10 3 18 5 1 12 20 15 19) nil -1))))
+  (is (= '(1 0 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0) (updateBoardHits '(22 13 17 11 0 8 2 23 4 24 21 9 14 16 7 6 10 3 18 5 1 12 20 15 19) '(1 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0) 17))))
 
 (deftest updateAllBoardHits-tests
   (is (= boardHitsTest (updateAllBoardHits boardData 17))))
+
+(deftest checkForBingoHor-test
+  (is (= true (checkForBingoHor '(0 0 0 0 0 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0))))
+  (is (= false (checkForBingoHor '(0 1 1 1 1 1 1 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)))))
+
+(deftest checkForBingoVer-test
+  (is (= true (checkForBingoVer '(0 1 0 0 0  1 1 1 1 1  0 1 0 0 0  0 1 0 0 0  0 1 0 0 0))))
+  (is (= false (checkForBingoVer '(0 1 1 1 1 1 1 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)))))
+
+(deftest checkForBingo-test
+  (is (= "board2" (checkForBingo boardHitsBingoHor)))
+  (is (= "board1" (checkForBingo boardHitsBingoVer))))
+
+(deftest processDrawnNumbers-boardData
+  (is (= boardHitsTest (processDrawnNumbers boardData '(7 4 9 5 11 17 23 2 0 14 21 24 10 16 13 6 15 25 12 22 18 20 8 19 3 26 1)))))
